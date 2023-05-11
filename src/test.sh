32e9de78044c413492e9391895bebc92dbe385a3
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2023-05-11 18:22:52 +0900

    feat: GitHub Actions

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Perform landscaper
-docker pull ghcr.io/chiba-ai-med/landscaper:main
+docker pull ghcr.io/kokitsuyuzaki/landscaper:main
 
-# docker run --rm -v $(pwd):/work ghcr.io/chiba-ai-med/tensorlycv:main \
+# docker run --rm -v $(pwd):/work ghcr.io/kokitsuyuzaki/tensorlycv:main \
 # -i /work/data/vaccine_tensor.npy -o /work/output \
 # --cores=2 --rank_min=2 --rank_max=2 --trials=2 \
 # --n_iter_max=2 --ratio=30 --memgb=100
