fad226925efad1c69d5da02740eb7c68b694f8e0
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2023-06-08 20:44:13 +0900

    feat: Docker

--- src/test.sh
+++ src/test.sh
@@ -5,5 +5,5 @@ docker run --rm -v $(pwd):/work ghcr.io/chiba-ai-med/landscaper:main \
 -i /work/data/testdata.tsv -o /work/output \
 --memgb=10
 
-# Test
+# Test Outputs
 docker run --rm -v $(pwd):/work koki/landscaper_component:latest Rscript /work/src/test.R
