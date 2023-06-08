d3468ccd785a6dbd423cb9c06f911b7da7acd6e3
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2023-06-08 20:39:05 +0900

    feat: Docker

--- src/test.R
+++ src/test.R
@@ -1,7 +1,7 @@
 library("testthat")
 
-test_file("src/test-estimate_ising.R")
-test_file("src/test-status_network.R")
-test_file("src/test-energy_barrier.R")
-test_file("src/test-dendrogram.R")
-test_file("src/test-figures.R")
+test_file("/work/src/test-estimate_ising.R")
+test_file("/work/src/test-status_network.R")
+test_file("/work/src/test-energy_barrier.R")
+test_file("/work/src/test-dendrogram.R")
+test_file("/work/src/test-figures.R")
