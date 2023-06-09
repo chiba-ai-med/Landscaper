d288b0f76debb22252ef8afe6c1199c20da4d526
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2023-06-09 09:13:54 +0900

    feat: Docker

--- src/test.R
+++ src/test.R
@@ -1,7 +1,16 @@
 library("testthat")
 
-test_file("/work/src/test-estimate_ising.R")
-test_file("/work/src/test-status_network.R")
-test_file("/work/src/test-energy_barrier.R")
-test_file("/work/src/test-dendrogram.R")
-test_file("/work/src/test-figures.R")
+test1 <- test_file("/work/src/test-estimate_ising.R")
+test2 <- test_file("/work/src/test-status_network.R")
+test3 <- test_file("/work/src/test-energy_barrier.R")
+test4 <- test_file("/work/src/test-dendrogram.R")
+test5 <- test_file("/work/src/test-figures.R")
+
+no_errors <- unlist(lapply(list(test1, test2, test3, test4, test5), length))
+
+target <- which(no_errors != 0)
+
+if(length(target) != 0){
+	msg <- paste0(paste(target, collapse=","), "-th tests return errors!!!!!!!")
+	stop(msg)
+}
