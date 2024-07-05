ddcd5977bcc276083141c00e5d9338ad6cfd42f6
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2024-07-05 15:48:17 +0900

    feat: covariance

--- src/test_cov.R
+++ src/test_cov.R
@@ -3,10 +3,9 @@ library("testthat")
 test1 <- test_file("/work/src/test-estimate_ising_cov.R")
 test2 <- test_file("/work/src/test-status_network_cov.R")
 test3 <- test_file("/work/src/test-energy_barrier_cov.R")
-test4 <- test_file("/work/src/test-dendrogram_cov.R")
-test5 <- test_file("/work/src/test-figures_cov.R")
+test4 <- test_file("/work/src/test-figures_cov.R")
 
-no_errors <- unlist(lapply(list(test1, test2, test3, test4, test5), length))
+no_errors <- unlist(lapply(list(test1, test2, test3, test4), length))
 
 target <- which(no_errors != 0)
 
